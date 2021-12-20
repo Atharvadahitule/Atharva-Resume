3cd352cf8362dc2b346c0728835c146f46de7a09
Atharva <atharvadahitule@gmail.com>
2021-12-20 09:38:09 +0530

    Updates

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi ="http://localhost:7071/api/GetResumeCounter";
+const functionApiUrl = "https://atharvaresumecounter.azurewebsites.net/api/GetResumeCounter?code=1Fe2l4E87jgUCQgmZNoy5qYQ2YqLXhKys4SCD227ApuM5WmlLY0x3Q==";
+const localFunctionApi ="http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi)
+    fetch(functionApiUrl)
     .then(response => {
         return response.json()
     })
