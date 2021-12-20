46077ac00c2d217350425da3474c8acb5d1481ed
Atharva <atharvadahitule@gmail.com>
2021-12-20 07:21:09 +0530

    Updates

--- frontend/main.js
+++ frontend/main.js
@@ -2,16 +2,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi ="http://localhost:7071/api/GetResumeCounter";
+const functionApiUrl =""
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi) .then(response => {
+    fetch(functionApi)
+    .then(response => {
         return response.json()
-    }) .then(response => {
+    })
+    .then(response => {
         console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
+        document.getElementById('counter').innerText = count;
     }).catch(function(error) {
         console.log(error);
       });
